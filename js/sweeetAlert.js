c907e227962f0c4172c0b077daa1e16f70c90954
AlexNuamah <alexandernuamahjr@gmail.com>
2023-10-03 12:19:26 +0000

    Update: integrating SweetAlert(popup notification)

--- js/sweeetAlert.js
+++ js/sweeetAlert.js
@@ -1,5 +1,31 @@
 const btnSave = document.querySelector('#btnSave');
 
 btnSave.addEventListener('click', ()=>{
-    alert('Hello World')
+    // Getting all inputs
+    const allInputs = document.querySelectorAll('.input-validator');
+    allInputs.forEach((input)=>{
+        if (input.value.length < 1){
+            // Error message
+        }
+    })
 });
+
+// Toast message function
+function showToast(status, message){
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        icon: 'success',
+        title: 'Signed in successfully'
+      })
+}
